8f9acd07516832b5600e89360c8a61fb01f4c94c
Iryna Popova <popovaira969@gmail.com>
2023-03-29 11:34:27 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import Notiflix from 'Notiflix';
+import { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
-import { Component } from 'react';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { GetImage } from '../../src/services/GetImage';
 import { Button } from './Button/Button';
@@ -8,99 +8,87 @@ import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 import css from './App.module.css';
 
-export class App extends Component {
-  state = {
-    value: '',
-    images: [],
-    error: null,
-    isLoader: false,
-    page: 1,
-    largeImageURL: '',
-    showModal: false,
-  };
-
-  componentDidUpdate(_, prevState) {
-    const { value, page } = this.state;
-    if (prevState.value !== value || prevState.page !== page) {
-      this.setState(({ isLoader }) => ({ isLoader: !isLoader }));
-      GetImage(value, page)
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          }
-          return Promise.reject(new Error('Not found'));
-        })
-        .then(images => {
-          if (images.hits.length > 0) {
-            this.setState(prevImages => ({
-              images:
-                page === 1
-                  ? images.hits
-                  : [...prevImages.images, ...images.hits],
-            }));
-          } else {
-            Notiflix.Notify.failure(
-              'Sorry, there are no images matching your search query. Please try again.'
-            );
-          }
-        })
-        .catch(error => this.setState({ error: error }))
-        .finally(() =>
-          this.setState(({ isLoader }) => ({ isLoader: !isLoader }))
-        );
+export const App = () => {
+  const [value, setValue] = useState('');
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoader, setLoader] = useState(false);
+  const [page, setPage] = useState(1);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [showModal, setShowModal] = useState(false);
+ 
+  useEffect(() => {
+    if (value === '') {
+      return;
     }
-    if (prevState.page !== page && page !== 1) {
-      this.setState(({ isLoader }) => ({ isLoader: !isLoader }));
-    }
-  }
+    setLoader(prevLoader => !prevLoader);
 
-  onSubmit = e => {
+    GetImage(value, page)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(new Error('Not found'));
+      })
+      .then(response => {
+        if (response.hits.length > 0) {
+          setImages(prevImages =>
+            page === 1 ? response.hits : [...prevImages, ...response.hits]
+          );
+        } else {
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        }
+      })
+      .catch(error => setError(error))
+      .finally(() => setLoader(false));
+  }, [value, page]);
+
+  const onSubmit = e => {
     e.preventDefault();
     const form = e.currentTarget;
-    this.setState({ value: form.elements.input.value, page: 1,images: [] });
+    setValue(form.elements.input.value);
+    setPage(1);
+    setImages([]);
     form.reset();
   };
 
-  onLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-      isLoader: true,
-    }));
+  const onLoadMore = () => {
+    setPage(page + 1);
   };
 
-  openModal = largeImageURL => {
-    this.setState({ largeImageURL: largeImageURL, showModal: true });
-  };
-  closeModal = e => {
-    if (e.target.nodeName === 'DIV') this.setState({ showModal: false });
-  };
-  closeESCModal = e => {
-    this.setState({ showModal: false });
+  const openModal = largeImageURL => {
+    setLargeImageURL(largeImageURL);
+    setShowModal(true);
   };
 
-  render() {
-    const { images, error, isLoader, showModal, largeImageURL } = this.state;
-    return (
-      <>
-        <div className={css.app}>
-          {error && <h1>{error.message}</h1>}
-          <Searchbar onSubmit={this.onSubmit} />
-          {images.length > 0 && (
-            <ImageGallery images={images} openModal={this.openModal} />
-          )}
-          {isLoader && <Loader />}
-          {!isLoader && images.length > 0 && (
-            <Button onLoadMore={this.onLoadMore} />
-          )}
-          {showModal && (
-            <Modal
-              closeESCModal={this.closeESCModal}
-              closeModal={this.closeModal}
-              largeImageURL={largeImageURL}
-            />
-          )}
-        </div>
-      </>
-    );
-  }
-}
+  const closeModal = e => {
+    if (e.target.nodeName === 'DIV') setShowModal(false);
+  };
+
+  const closeESCModal = e => {
+    setShowModal(false);
+  };
+
+  return (
+    <>
+      <div className={css.app}>
+        {error && <h1>{error.message}</h1>}
+        <Searchbar onSubmit={onSubmit} />
+        {images.length > 0 && (
+          <ImageGallery images={images} openModal={openModal} />
+        )}
+        {isLoader && <Loader />}
+        {!isLoader && images.length > 0 && <Button onLoadMore={onLoadMore} />}
+        {showModal && (
+          <Modal
+            closeESCModal={closeESCModal}
+            closeModal={closeModal}
+            largeImageURL={largeImageURL}
+          />
+        )}
+      </div>
+    </>
+  );
+};
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,11 +3,12 @@ import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import css from './ImageGallery.module.css';
 
 export const ImageGallery = ({ images, openModal }) => {
+
   return (
     <ul className={css.gallery}>
       {images.map(image => {
         const { id, webformatURL, largeImageURL, tags } = image;
-        return (
+               return (
           <ImageGalleryItem
             largeImageURL={largeImageURL}
             openModal={openModal}
